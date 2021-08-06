d402e7d99155c73fc889d4b4d5daa580cfcfbf72
iii8iii <zichengou@gmail.com>
2021-08-06 13:54:02 +0800

    done

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,55 @@
-export const sum = (a: number, b: number) => {
-  if ('development' === process.env.NODE_ENV) {
-    console.log('boop');
+import got from 'got';
+import dayjs from 'dayjs';
+
+export const sendMsg = async (
+  msg: string | object,
+  botUrls: string[],
+  preItem = 0
+) => {
+  try {
+    let item = preItem;
+    //the second round to 0
+    if (preItem >= botUrls.length) {
+      item = 0;
+    }
+    //if there is no URL offered, do nothing and return just in case
+    if (!botUrls.length) {
+      return;
+    }
+    //manage the message
+    if (typeof msg === 'object') {
+      msg = JSON.stringify(msg);
+    }
+    const { heapTotal, heapUsed } = process.memoryUsage();
+    const defaultMsgContent = `-------------------------\n>Total:${(
+      heapTotal /
+      1024 /
+      1024 /
+      1024
+    ).toFixed(2)} G\n>Used:${((heapUsed / heapTotal) * 100).toFixed(
+      2
+    )}%\n>${dayjs().format('YYYYMMDD HH:mm:ss')} ~`;
+
+    let botUrl = botUrls[item];
+
+    const { body } = await got.post(botUrl, {
+      json: {
+        msgtype: 'markdown',
+        markdown: {
+          content: `${msg}\n${defaultMsgContent} `,
+        },
+      },
+    });
+
+    const { errcode } = JSON.parse(body);
+    if (errcode) {
+      if (preItem >= botUrls.length) {
+        return;
+      }
+      preItem += 1;
+      await sendMsg(msg, botUrls, preItem);
+    }
+  } catch (err) {
+    console.error('ERROR OCCURED IN SENDMSG');
   }
-  return a + b;
 };
---
+++ test/sendMsg.test.ts
@@ -0,0 +1,22 @@
+import { sendMsg } from '../src/index';
+describe('sendMsg', () => {
+  const urls = [
+    'https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=1ee8d91c-ee26-458f-9913-fd0d4eb585a7',
+    'https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=0ecd18fa-d56c-4938-a984-4d91bdc06e37',
+  ];
+  it('send a message', () => {
+    sendMsg('this is a message from test', urls);
+  });
+  it('send a message when the URL is incorrect or forbidden by WeChat', async () => {
+    const p = new Promise<void>(resolve => {
+      let a = setInterval(async () => {
+        await sendMsg('message forbidden', urls, 1);
+      }, 100);
+      setTimeout(() => {
+        clearInterval(a);
+        resolve();
+      }, 5000);
+    });
+    await p;
+  });
+});
